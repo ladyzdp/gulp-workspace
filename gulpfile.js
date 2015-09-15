54ffb7a6142904377285ab31a75a65c2f641e2a1
huangzhengzheng <sassplus@163.com>
2015-09-15 17:58:41 +0800

    深度压缩修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    compass = require('gulp-compass'),
-    tinypng = require('gulp-tinypng');
-// imagemin = require('gulp-imagemin');
-// pngquant = require('imagemin-pngquant');
+var gulp = require('gulp'), //基础库
+    plumber = require('gulp-plumber'), //错误跳出
+    compass = require('gulp-compass'), //编译sass
+    tinypng = require('gulp-tinypng'), //图片压缩tinypng
+    imagemin = require('gulp-imagemin'), //图片压缩
+    pngquant = require('imagemin-pngquant'); //深度压缩
+    // cache = require('gulp-cache'); // 文件清理
+
 // compass
 gulp.task('compass', function() {
 
@@ -23,31 +25,45 @@ gulp.task('compass', function() {
         .pipe(gulp.dest('css'));
 });
 //图片压缩
-// gulp.task('imagemin', function() {
-//     return gulp.src('./images/*')
-//         .pipe(imagemin({
-//             progressive: true,
-//             svgoPlugins: [{
-//                 removeViewBox: false
-//             }],
+gulp.task('imagemin', function() {
+    gulp.src('./images/*')
+        .pipe(imagemin({
+            progressive: true, // 无损压缩JPG图片
+            svgoPlugins: [{
+                removeViewBox: false
+            }], // 不要移除svg的viewbox属性
+            use: [pngquant()] // 使用pngquant插件进行深度压缩
+        }))
 
-//         }))
-//         .pipe(gulp.dest('./images'));
+    .pipe(gulp.dest('build/images'));
+});
+//深度压缩
+// gulp.task('pngquant', function() {
+//     gulp.src('./images/*.png')
+//         .pipe(pngquant({
+//             quality: '65-80',
+//             speed: 4
+//         })())
+//         .pipe(gulp.dest('build/images'));
 // });
 
 //tinypng图片压缩
-gulp.task('tinypng', function() {
-    gulp.src('./images/**/*.*')
-        .pipe(tinypng('hborAzV1UAecTvoPizaB91UQnyRuvowC'))
-        .pipe(gulp.dest('images'));
-});
+// gulp.task('tinypng', function() {
+//     gulp.src('./images/*')
+//         .pipe(tinypng('hborAzV1UAecTvoPizaB91UQnyRuvowC'))
+//         .pipe(gulp.dest('images'));
+// });
+//缓存清理
+// gulp.task('clean', function(done) {
+//     return cache.clearAll(done);
+// });
 // 监听
 gulp.task('watch', function() {
-
-    
-    gulp.watch('./sass/*/*.scss', ['compass']);// 监听所有.scss档
-    gulp.watch('./config.b', ['compass']);//监听confirg.rg
-    gulp.watch('./images/**/*.*', ['tinypng']);//监听图片改动
+    gulp.watch('./sass/*/*.scss', ['compass']); // 监听所有.scss档
+    gulp.watch('./config.b', ['compass']); //监听confirg.rg
+    gulp.watch('./images/*', ['imagemin']); //监听图片改动
+    //gulp.watch('./images/*', ['pngquant']); //监听图片改动
+    //gulp.watch('./images/*', ['tinypng']); //监听图片改动
 
     // // 监听所有.js档
     // gulp.watch('src/scripts/**/*.js', ['scripts']);
@@ -65,4 +81,4 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('default', ['compass', 'watch', 'tinypng']);
+gulp.task('default', ['compass', 'watch', 'imagemin']);
