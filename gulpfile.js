5224e06ad4bbbe7291ab5712594d7943e2f32f9f
sassplus <sassplus@163.com>
2015-09-17 18:11:36 +0800

    添加几个gulp插件

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,58 @@
 var gulp = require('gulp'), //基础库
+    usemin = require('gulp-usemin'), //usemin
     plumber = require('gulp-plumber'), //错误跳出
     compass = require('gulp-compass'), //编译sass
     tinypng = require('gulp-tinypng'), //图片压缩tinypng
     imagemin = require('gulp-imagemin'), //图片压缩
-    // gulpif = require('gulp-if'),
-    // sprity = require('sprity'),
-    spritesmith = require('gulp.spritesmith'),
+    //bower = require('gulp-bower'),
     pngquant = require('imagemin-pngquant'); //深度压缩
-// cache = require('gulp-cache'); // 文件清理
-//sprity
+//gulpif = require('gulp-if'),
+//sprity = require('sprity');
+//spritesmith = require('gulp.spritesmith'); //合并精灵图
+//cache = require('gulp-cache'); // 文件清理
+
+
+//sprity合并精灵图
 // gulp.task('sprites', function() {
 //     return sprity.src({
-//             src: './src/images/**/*.{png}',
+//             src: './src/images/**/*.{png,jpg}',
 //             style: './sprite.css',
 //             // ... other optional options 
 //             // for example if you want to generate scss instead of css 
 //             processor: 'sass', // make sure you have installed sprity-sass 
 //         })
-//         .pipe(gulpif('*.png', gulp.dest('./dist/img/'), gulp.dest('./dist/css/')))
+//         .pipe(gulpif('*.png', gulp.dest('./dist/img/'), gulp.dest('./dist/css/')));
 // });
-gulp.task('sprite', function() {
+
+//usemin
+gulp.task('usemin', function() {
+    return gulp.src('./*.html')
+        .pipe(usemin({
+            css: [rev()],
+            html: [minifyHtml({
+                empty: true
+            })],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [minifyCss(), 'concat']
+        }))
+        .pipe(gulp.dest('build/'));
+});
+//gulp.spritesmith合并精灵图
+/*gulp.task('sprite', function() {
     var spriteData = gulp.src('images/icons/*.png').pipe(spritesmith({
         imgName: 'sprite.png',
         cssName: 'sprite.css',
-        padding: 10
-
-
+        padding: 20,
     }));
+
+    //spriteData.img.pipe(gulp.dest("./css")); // 雪碧图输出路径
+    //spriteData.css.pipe(gulp.dest("./css")); //css输出路径
     return spriteData.pipe(gulp.dest('path/to/output/'));
-});
-// compass
+});*/
+
+
+// compass编译scss
 gulp.task('compass', function() {
 
     gulp.src('./sass/**/*.*')
@@ -47,6 +70,8 @@ gulp.task('compass', function() {
         }))
         .pipe(gulp.dest('css'));
 });
+
+
 //图片压缩
 gulp.task('imagemin', function() {
     gulp.src('./images/*')
@@ -58,28 +83,35 @@ gulp.task('imagemin', function() {
             use: [pngquant()] // 使用pngquant插件进行深度压缩
         }))
 
-    .pipe(gulp.dest('build/images'));
+    .pipe(gulp.dest('build/images')); //压缩后的图片存放路径
 });
+
+
 //深度压缩
-// gulp.task('pngquant', function() {
-//     gulp.src('./images/*.png')
-//         .pipe(pngquant({
-//             quality: '65-80',
-//             speed: 4
-//         })())
-//         .pipe(gulp.dest('build/images'));
-// });
+/*gulp.task('pngquant', function() {
+    gulp.src('./images/*.png')
+        .pipe(pngquant({
+            quality: '65-80',
+            speed: 4
+        })())
+        .pipe(gulp.dest('build/images'));
+});*/
+
 
 //tinypng图片压缩
-// gulp.task('tinypng', function() {
-//     gulp.src('./images/*')
-//         .pipe(tinypng('hborAzV1UAecTvoPizaB91UQnyRuvowC'))
-//         .pipe(gulp.dest('images'));
-// });
+/*gulp.task('tinypng', function() {
+    gulp.src('./images/*')
+        .pipe(tinypng('hborAzV1UAecTvoPizaB91UQnyRuvowC'))
+        .pipe(gulp.dest('images'));
+});*/
+
+
 //缓存清理
-// gulp.task('clean', function(done) {
-//     return cache.clearAll(done);
-// });
+/*gulp.task('clean', function(done) {
+    return cache.clearAll(done);
+});*/
+
+
 // 监听
 gulp.task('watch', function() {
     gulp.watch('./sass/**/*.*', ['compass']); // 监听所有.scss文件
@@ -104,4 +136,4 @@ gulp.task('watch', function() {
 
 });
 
-gulp.task('default', ['compass', 'watch', 'imagemin', 'sprite']);
+gulp.task('default', ['compass', 'watch', 'imagemin', 'usemin']);
