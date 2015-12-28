a507e8685201487910037384fb16bd6664b0bd6e
sassplus <sassplus@163.com>
2015-12-28 15:01:54 +0800

    优化gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,25 @@
 var gulp = require('gulp'), //基础库
-  clean = require('gulp-clean'),
-  autoprefixer = require('gulp-autoprefixer'),
-  plumber = require('gulp-plumber'), //错误跳出
-  compass = require('gulp-compass'), //编译sass
-  tinypng = require('gulp-tinypng'),
-  gulpif = require('gulp-if'),
-  livereload = require('gulp-livereload');
+  gulpLoadPlugins = require('gulp-load-plugins'),
+  plugins = gulpLoadPlugins();
 
-livereload({
+
+plugins.livereload({
     start: true
   })
   //配置路径
 var config = {
-  cssUrl: 'assets/css/*.css', //css路径
-  scssUrl: 'assets/sass/*/*.scss', //scss路径
+  cssUrl: 'assets/css/*.css',
+  scssUrl: 'assets/sass/*/*.scss',
   jsUrl: 'assets/js/*.js', //js路径
-  imagesUrl: 'assets/images/*.{png,jpg}', //图片路径
-  htmlUrl: '*.html' //html路径
+  imagesUrl: 'assets/images/*.{png,jpg}',
+  htmlUrl: '*.html'
 
 };
 gulp.task('clean', function() {
   return gulp.src('assets', {
       read: false
     })
-    .pipe(clean());
+    .pipe(plugins.clean());
 });
 
 
@@ -31,24 +27,24 @@ gulp.task('clean', function() {
 gulp.task('compass', function() {
 
   return gulp.src(config.scssUrl)
-    .pipe(plumber({
+    .pipe(plugins.plumber({
       errorHandler: function(error) {
         console.log(error.message);
         this.emit('end');
       }
     }))
-    .pipe(compass({
+    .pipe(plugins.compass({
       config_file: './config.rb',
       css: 'assets/css',
       sass: 'assets/sass',
       images: 'assets/images'
     }))
-    .pipe(autoprefixer({
+    .pipe(plugins.autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(gulp.dest('assets/css'))
-    .pipe(livereload());
+    .pipe(plugins.livereload());
 
 });
 
@@ -56,17 +52,15 @@ gulp.task('compass', function() {
 gulp.task('tinypng', function() {
   return gulp.src(config.imagesUrl)
     //tinypng图片压缩
-    .pipe(tinypng('m66cergQwJ-L96d3X1QhVs-mQs8WzrPm'))
-    .pipe(gulp.dest('assets/images')); //压缩后的图片存放路径
+    .pipe(plugins.tinypng('m66cergQwJ-L96d3X1QhVs-mQs8WzrPm'))
+    .pipe(gulp.dest('assets/images'));
 });
 
 
 
 // 监听
 gulp.task('watch', function() {
-      gulp.watch([config.scssUrl, 'config.rb'], ['compass']); // 监听所有.scss文件监听confirg.rb
-
-      gulp.watch([config.imagesUrl, config.cssUrl, config.htmlUrl].on('change', livereload.changed)
-      });
-
-    gulp.task('default', ['compass', 'tinypng', 'watch']);
+  gulp.watch([config.scssUrl, 'config.rb'], ['compass']);
+  gulp.watch([config.imagesUrl, config.cssUrl, config.htmlUrl]).on('change', plugins.livereload.changed);
+});
+gulp.task('default', ['compass', 'tinypng', 'watch']);
