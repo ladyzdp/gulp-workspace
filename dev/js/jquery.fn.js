012f30fbe8df9837770c255541e43033b6b8bcb1
sassplus <sassplus@163.com>
2016-01-15 18:05:31 +0800

    edit jquery.fn.js

--- dev/js/jquery.fn.js
+++ dev/js/jquery.fn.js
@@ -2,9 +2,18 @@
 (function($) {
 
   $.fn.extend({
-      "color": function(value) {
-        return this.css("color", value);
-      });
-  }
+    "color": function(value) {
+      return this.css("color", value);
+    },
+    "tableBgClolr": function(option) {
+      option = $.extend({
+        odd: "odd",
+        even: "even",
+        selected: "selected"
 
-})(jQuery);
+      }, option);
+
+    }
+  });
+
+})(jQuery)
