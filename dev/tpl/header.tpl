bcb1efe588aa27a8072aa28747784d9ae4948910
sassplus <huangzheng326@gmail.com>
2016-05-11 17:51:33 +0800

    '增加一些功能'

---
+++ dev/tpl/header.tpl
@@ -0,0 +1,6 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1.0, user-scalable=0">
+    <title>@@title</title>
+    <link rel="stylesheet" href="../assets/css/common.css">
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var gulp = require('gulp'),
     merge = require('merge-stream'),
     gulpSequence = require('gulp-sequence'),
     cleanCSS = require('gulp-clean-css'),
+    fileinclude = require('gulp-file-include'),
+    htmlmin = require('gulp-htmlmin'),
+    md5 = require('gulp-md5-plus'),
     $ = gulpLoadPlugins();
 
 
@@ -12,31 +15,43 @@ $.livereload({
 })
 
 //配置路径
-var baseUrl = './dev/assets/';
+var baseUrl = './dev/';
+var distUrl = './app/';
 var tinypngApi = 'm66cergQwJ-L96d3X1QhVs-mQs8WzrPm';
 var configUrl = {
-    assets: {
-        css: baseUrl + 'css/*.css',
-        scss: baseUrl + 'sass/**/*.scss',
+    file: {
+        css: baseUrl + 'assets/css/*.css',
+        scss: baseUrl + 'assets/sass/**/*.scss',
+        images: baseUrl + 'assets/images/*.{png,jpg}',
         js: baseUrl + 'js/*.js',
-        images: baseUrl + 'images/*.{png,jpg}',
-        html: './dev/static/*.html'
+        html: baseUrl + 'static/*.html',
+        htmlfile: baseUrl + 'html/*.html',
+    },
+    folder: {
+        css: distUrl + 'assets/css',
+        images: baseUrl + 'assets/images',
+        scss: baseUrl + 'assets/sass',
+        sprites: baseUrl + 'assets/sass/sprites',
+        js: baseUrl + 'js',
+        html: baseUrl + 'html'
+
     },
     dist: {
-        css: baseUrl + 'css/',
-        images: baseUrl + 'images/',
-        js: baseUrl + 'js/',
-        scss: baseUrl + 'sass/',
-        sprites: baseUrl + 'sass/sprites/'
-
+        css: distUrl + 'assets/css',
+        images: distUrl + 'assets/images',
+        scss: distUrl + 'assets/sass',
+        sprites: distUrl + 'assets/sass/sprites',
+        js: distUrl + 'js',
+        html: distUrl + 'html'
     }
 };
 
+
+
 //清除文件
 gulp.task('clean', function() {
-    return gulp.src('assets', {
-            read: false
-        })
+    return gulp.src('./app')
+        .pipe($.clean())
         .pipe($.clean());
 });
 
@@ -57,10 +72,10 @@ gulp.task('sprites', function() {
 
     var imgStream = spriteData.img
         .pipe(buffer())
-        .pipe(gulp.dest(configUrl.dist.images))
+        .pipe(gulp.dest(configUrl.folder.images))
 
     var cssStream = spriteData.css
-        .pipe(gulp.dest(configUrl.dist.sprites))
+        .pipe(gulp.dest(configUrl.folder.sprites))
 
     //生成多个精灵图
     var spirteFile = gulp.src('./dev/assets/images/icons1/*.png')
@@ -87,53 +102,97 @@ gulp.task('sprites', function() {
 
     var imgFood = spirteFile.img
         .pipe(buffer())
-        .pipe(gulp.dest(configUrl.dist.images))
+        .pipe(gulp.dest(configUrl.folder.images))
 
     var cssFood = spirteFile.css
-        .pipe(gulp.dest(configUrl.dist.sprites))
+        .pipe(gulp.dest(configUrl.folder.sprites))
 
     return merge([imgStream, cssStream, imgFood, cssFood]);
 });
 
 //sass编译
 gulp.task('sass', function() {
-    return gulp.src(configUrl.assets.scss)
+    return gulp.src(configUrl.file.scss)
         .pipe($.sourcemaps.init())
         .pipe($.sass({ outputStyle: 'compressed' }).on('error', $.sass.logError))
         .pipe($.sourcemaps.write('./'))
-        .pipe(gulp.dest(configUrl.dist.css))
+        .pipe(gulp.dest(configUrl.folder.css))
         .pipe($.livereload());
 });
 
 
-
-
-
 //压缩排序优化CSS
 gulp.task('minicss', function() {
-    return gulp.src(configUrl.assets.css)
+    return gulp.src(configUrl.file.css)
         .pipe($.autoprefixer())
         .pipe($.csscomb())
         .pipe($.csso())
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest(configUrl.dist.css));
+        .pipe(gulp.dest(configUrl.dist.css))
+        .pipe($.livereload());
+});
+
+//加MD5
+gulp.task('md5:css', ['minicss'], function(done) {
+    gulp.src(configUrl.file.css)
+        .pipe(md5(10, './app/html/*.html'))
+        .pipe(gulp.dest(configUrl.folder.css))
+        .pipe($.livereload())
+        .on('end', done);
+});
+
+
+gulp.task('minifyjs', function() {
+    return gulp.src(configUrl.file.js) //需要操作的文件
+        //.pipe($.concat('main.js'))    //合并所有js到main.js
+        //.pipe(gulp.dest('js'))       //输出到文件夹
+        //.pipe($.rename({suffix: '.min'}))   //rename压缩后的文件名
+        .pipe($.uglify()) //压缩
+        .pipe(gulp.dest(configUrl.dist.js)); //输出
 });
 
 //tinypng图片压缩
 gulp.task('tinypng', function() {
-    return gulp.src(configUrl.assets.images)
+    return gulp.src(configUrl.file.images)
         .pipe($.cache($.tinypng(tinypngApi)))
-        .pipe(gulp.dest(configUrl.dist.images));
+        .pipe(gulp.dest(configUrl.dist.images))
+        .pipe($.livereload());
 });
 
+//fileinclude
+gulp.task('fileinclude', function() {
+    gulp.src(configUrl.file.html)
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest(configUrl.folder.html));
+});
+
+//压缩html
+gulp.task('htmlmin', function() {
+    return gulp.src(configUrl.file.htmlfile)
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(configUrl.dist.html))
+        .pipe($.livereload());
+});
 
 
 // 监听
 gulp.task('watch', function() {
-    //gulp.watch(configUrl.assets.scss, ['sass']).on('change', $.livereload.changed);
-    gulp.watch(configUrl.assets.scss, ['sass', 'minicss']);
-    gulp.watch(['./dev/**/*.*', '*.html']).on('change', function() { $.livereload.changed });
+    //gulp.watch(configUrl.file.scss, ['sass']).on('change', $.livereload.changed);
+    $.livereload.listen();
+    gulp.watch(configUrl.file.scss, ['sass', 'minicss']);
+    gulp.watch(configUrl.file.html, ['fileinclude']);
+    gulp.watch('./dev/html/*.html', ['htmlmin']);
+
+    gulp.watch(['./dev/**/*.{html,js,css,scss,jpg,png}', '*.html', './app/**/*.{html,js,css,scss,jpg,png}']).on('change', function() {
+        $.livereload.changed
+        $.livereload.changed
+    });
 });
 
-// gulp.task('default', ['compass','minicss' ,'tinypng', 'watch']);
-gulp.task('default', gulpSequence('sprites', 'sass', 'minicss', 'tinypng', 'watch'));
+// 发布
+gulp.task('default', gulpSequence('clean', 'sprites', 'sass', 'minicss', 'tinypng', 'fileinclude', 'htmlmin', 'md5:css', 'watch'));
+//开发
+gulp.task('dev', gulpSequence('clean', 'sprites', 'sass', 'minicss', 'minifyjs', 'tinypng', 'fileinclude', 'htmlmin', 'watch'));
