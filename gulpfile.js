c105c414a722c51c844236e3a45789140140d73e
sassplus <huangzheng326@gmail.com>
2017-03-03 15:00:33 +0800

    解决冲突

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,7 @@
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-<<<<<<< HEAD
-var gulp = require('gulp'),
-    gulpLoadPlugins = require('gulp-load-plugins'),
-    buffer = require('vinyl-buffer'),
-    merge = require('merge-stream'),
-    gulpSequence = require('gulp-sequence'),
-    cleanCSS = require('gulp-clean-css'),
-    livereload = require('gulp-livereload'),
-    webserver = require('gulp-webserver'),
-    fileinclude = require('gulp-file-include'),
-    $ = gulpLoadPlugins();
-=======
+
 var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var buffer = require('vinyl-buffer');
@@ -24,7 +13,7 @@ var livereload = require('gulp-livereload');
 var webserver = require('gulp-webserver');
 var fileinclude = require('gulp-file-include');
 var $ = gulpLoadPlugins();
->>>>>>> dev
+
 
 
 livereload({ start: true, auto: false });
@@ -129,11 +118,7 @@ gulp.task('sprites', function() {
 //sass编译
 gulp.task('sass', function() {
     return gulp.src(configUrl.file.scss)
-<<<<<<< HEAD
-        .pipe($.sourcemaps.init({ loadMaps: true }))
-        .pipe($.sass().on('error', $.sass.logError))
-        // .pipe($.autoprefixer())
-=======
+
 .pipe($.sourcemaps.init({ loadMaps: true }))
     .pipe($.sass().on('error', $.sass.logError))
         .pipe($.autoprefixer()) //添加前缀
@@ -143,13 +128,13 @@ gulp.task('sass', function() {
             html: [configUrl.file.htmlfile] //css清理
         }))
         
->>>>>>> dev
+
         .pipe($.sourcemaps.write('./'))
         .pipe(gulp.dest(configUrl.folder.css))
         .pipe(livereload());
 });
 
-<<<<<<< HEAD
+
 //压缩排序优化CSS
 gulp.task('minicss', function() {
     return gulp.src(configUrl.file.css)
@@ -159,8 +144,7 @@ gulp.task('minicss', function() {
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest(configUrl.folder.css));
 });
-=======
->>>>>>> dev
+
 
 
 
@@ -181,11 +165,9 @@ gulp.task('fileinclude', function() {
             basepath: '@file'
         }))
         .pipe(gulp.dest(configUrl.folder.html))
-<<<<<<< HEAD
+
         .pipe(livereload());
-=======
-      
->>>>>>> dev
+
 });
 
 // webserver
@@ -239,18 +221,9 @@ gulp.task('watch', function() {
 });
 
 // 发布
-<<<<<<< HEAD
-<<<<<<< HEAD
-gulp.task('default', gulpSequence('clean', 'fileinclude', 'sass', 'minicss', 'webserver', 'watch'));
-=======
-gulp.task('default', gulpSequence('clean', 'sass',  'tinypng', 'fileinclude','watch'));
->>>>>>> 8b5b6689cdac9d1c778e4b7b679c424c08b07d61
-//开发
-gulp.task('dev', gulpSequence('clean', 'sprites', 'sass', 'minicss', 'minifyjs', 'tinypng', 'fileinclude', 'htmlmin', 'md5:css', 'watch'));
-=======
 
 gulp.task('default', gulpSequence('clean', 'fileinclude', 'sass', 'webserver', 'watch'));
 
 //开发
 gulp.task('dev', gulpSequence('clean', 'sprites', 'sass', 'minifyjs', 'tinypng', 'fileinclude', 'htmlmin', 'md5:css', 'watch'));
->>>>>>> dev
+
